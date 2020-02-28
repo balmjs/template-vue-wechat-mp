d12c77ef0086cbfad9f060f3467197b7ff74b272
Elf-mousE <ifmouz@gmail.com>
2020-02-28 12:28:06 +0800

    update

--- templates/babel.config.js
+++ templates/babel.config.js
@@ -7,10 +7,5 @@ module.exports = {
       }
     ]
   ],
-  plugins: ['@babel/plugin-transform-runtime'],
-  env: {
-    test: {
-      presets: ['@babel/preset-env']
-    }
-  }
+  plugins: ['@babel/plugin-transform-runtime']
 };
