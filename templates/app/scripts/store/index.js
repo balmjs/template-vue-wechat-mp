9b704da9d69a7aea1aa42f30d9195f1acca21dbd
Elf-mousE <ifmouz@gmail.com>
2022-05-01 16:06:34 +0800

    chore: update

--- templates/app/scripts/store/index.js
+++ templates/app/scripts/store/index.js
@@ -2,11 +2,6 @@ import demo from './modules/demo';
 
 export default {
   mixins: [demo],
-  data() {
-    return {
-      router: new Map()
-    };
-  },
   methods: {
     onClickJump(url) {
       window.location.href = url;
---
+++ templates/balm.env.js
@@ -0,0 +1,9 @@
+function setBalmCore(useSource = false) {
+  const BALM_ROOT = '/Users/elf-mouse/www/balmjs/balm';
+  const BALM_CORE_SOURCE = `${BALM_ROOT}/packages/balm-core`;
+  const BALM_CORE = '/usr/local/node_global/lib/node_modules/balm-core';
+
+  process.env.BALM_CORE = useSource ? BALM_CORE_SOURCE : BALM_CORE;
+}
+
+// setBalmCore();
